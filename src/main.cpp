8a18f5a15d05f8e825fbe788a9c66993fd3148ac
coding610 <sixten.bohman.08@gmail.com>
2024-02-01 15:27:01 +0100

    raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+#include <raylib.h>
 
 
 int main () {
-    std::cout << "Hello, World!\n";
+    float ratio = 16.0 / 9.0;
+    float height = 1000;
+    float width = ratio * height;
+    InitWindow(width, height, "");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            ClearBackground(BLACK);
+            DrawCircle(500, 500, 300, BLUE);
+        EndDrawing();
+    }
+
     return 0;
 }
